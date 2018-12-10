c654815d6e25d97b4d46635f091c46db34108751
Brian Golden <brian.golden09@gmail.com>
2018-12-09 16:07:48 -0800

    Add setup for simple use of Git on the command line

---
+++ GitSetup/MacInstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+wget -o ~/.gitconfig "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/EasyGit/gitconfig" 
+wget -o ~/.os.gitconfig "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/EasyGit/gitconfig-windows"
