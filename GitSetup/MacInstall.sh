619861175c137597737b324cf589e055cb5fa892
Brian Golden <brian.golden09@gmail.com>
2019-01-30 17:18:13 -0800

    Fix paths to gitconfig files in scripts

--- GitSetup/MacInstall.sh
+++ GitSetup/MacInstall.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-wget -o ~/.gitconfig "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/EasyGit/gitconfig" 
-wget -o ~/.os.gitconfig "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/EasyGit/gitconfig-windows"
+wget -o ~/.gitconfig "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/GitSetup/gitconfig" 
+wget -o ~/.os.gitconfig "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/GitSetup/gitconfig-windows"
--- GitSetup/WindowsInstall.ps1
+++ GitSetup/WindowsInstall.ps1
@@ -1,9 +1,9 @@
 # Install git things on Windows
 
 Invoke-WebRequest -OutFile $env:USERPROFILE\.gitconfig `
-  -Uri "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/EasyGit/gitconfig" 
+  -Uri "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/GitSetup/gitconfig" 
 Invoke-WebRequest -OutFile $env:USERPROFILE\.os.gitconfig `
-  -Uri "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/EasyGit/gitconfig-windows"
+  -Uri "https://raw.githubusercontent.com/HolyNamesAcademy/PostAPPlusPlusClassResources/master/GitSetup/gitconfig-windows"
 
 # Install posh-git
 Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
